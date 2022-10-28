ba1bcdfefdf250bbb0d18c45e6f81ff5185b1f6d
vini00784 <vo70346@gmail.com>
2022-10-28 09:39:57 -0300

    configuring the game class in the database

--- app/src/main/java/br/senai/sp/jandira/games/model/Game.kt
+++ app/src/main/java/br/senai/sp/jandira/games/model/Game.kt
@@ -1,10 +1,19 @@
 package br.senai.sp.jandira.games.model
 
 import android.graphics.drawable.Drawable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "tbl_game")
 class Game {
+
+    @PrimaryKey(autoGenerate = true)
     var code = 0
+
+    @ColumnInfo(name = "game_name")
     var gameName = ""
+
     var developer = ""
     var description = ""
     var image: Drawable? = null
