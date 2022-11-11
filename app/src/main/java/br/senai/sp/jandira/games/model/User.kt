f8039f72d44b9051db6327f2c37e82714acd93a8
vini00784 <vo70346@gmail.com>
2022-11-11 08:53:12 -0300

    creating user class

---
+++ app/src/main/java/br/senai/sp/jandira/games/model/User.kt
@@ -0,0 +1,40 @@
+package br.senai.sp.jandira.games.model
+
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "tbl_user")
+class User {
+
+    @PrimaryKey(autoGenerate = true)
+    var code = 0
+
+    @ColumnInfo(name = "user_name")
+    var userName = ""
+
+    var sex: Char? = null
+
+    @ColumnInfo(name = "user_city")
+    var userCity = ""
+
+    @ColumnInfo(name = "user_email")
+    var userEmail = ""
+
+    @ColumnInfo(name = "user_password")
+    var userPassword = ""
+
+    @ColumnInfo(name = "birth_date")
+    var birthDate: LocalDate? = null
+
+    @ColumnInfo(name = "user_image")
+    var userImage: Bitmap? = null
+
+    var console: Console? = null
+
+    @ColumnInfo(name = "user_level")
+    var userLevel: Levels? = null
+}
