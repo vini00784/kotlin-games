5daeb6bd087c5b8406045929fd5c4fb9db41652f
vini00784 <vo70346@gmail.com>
2022-11-18 07:40:56 -0300

    DAO to game class

---
+++ app/src/main/java/br/senai/sp/jandira/games/dao/game/GameDao.kt
@@ -0,0 +1,24 @@
+package br.senai.sp.jandira.games.dao.game
+
+import androidx.room.*
+import br.senai.sp.jandira.games.model.Game
+
+@Dao
+interface GameDao {
+
+    @Insert
+    fun save(game: Game): Long
+
+    @Delete
+    fun delete(game: Game): Int
+
+    @Update
+    fun update(game: Game): Int
+
+    @Query("SELECT * FROM tbl_game ORDER BY game_name ASC")
+    fun getAll(): List<Game>
+
+    @Query("SELECT * FROM tbl_game WHERE code = :id")
+    fun getGameById(id: Int): Game
+
+}
