7b876680c5417376509141db4d091484ee1eef1c
vini00784 <vo70346@gmail.com>
2022-11-25 09:55:17 -0300

    creating user profile

--- app/src/main/java/br/senai/sp/jandira/games/ui/GamesListActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/games/ui/GamesListActivity.kt
@@ -7,10 +7,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import br.senai.sp.jandira.games.R
 import br.senai.sp.jandira.games.adapter.GamesAdapter
 import br.senai.sp.jandira.games.databinding.ActivityGamesListBinding
+import br.senai.sp.jandira.games.model.User
+import br.senai.sp.jandira.games.repository.UserRepository
 
 class GamesListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGamesListBinding
+    lateinit var userRepository: UserRepository
+    var user: User? = null
 
     lateinit var adapterGames: GamesAdapter
 
@@ -19,6 +23,8 @@ class GamesListActivity : AppCompatActivity() {
         binding = ActivityGamesListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        createProfile()
+
         var rvGames = binding.rvGames
         rvGames.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
@@ -26,6 +32,19 @@ class GamesListActivity : AppCompatActivity() {
 //        adapterGames.updateGamesList(GamesDao.getGames(this))
 
         rvGames.adapter = adapterGames
+
+    }
+
+    private fun createProfile() {
+        val userId = intent.getIntExtra("userId", 0)
+
+        userRepository = UserRepository(this)
+        user = userRepository.getUserById(userId)
+
+        binding.textViewUsername.text = user?.userName
+        binding.textViewEmail.text = user?.userEmail
+        binding.gamesTextViewLevel.text = user?.userLevel.toString()
+
 
     }
 
